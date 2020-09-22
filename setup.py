5753a9f87b4130a6dd653d3e9b5f4a91642f2469
srmnitc <sarath.r.menon@gmail.com>
2020-09-22 14:55:41 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/srmnitc/pytint',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
