39f635ff9046d0ee06eb1df63fc6d6b5c578a931
srmnitc <sarathmenon@mailbox.org>
2020-10-26 22:53:29 +0100

    fix tabs in tests

--- tests/test_liquid_avg.py
+++ tests/test_liquid_avg.py
@@ -20,19 +20,19 @@ def test_liquid_averaging():
     assert np.abs(lqd.eps - 4.308666631) < 1E-2
 
     lqd.run_integration(iteration=1)
-	assert os.path.exists("forward_1.dat") == True
-	assert os.path.exists("backward_1.dat") == True
+    assert os.path.exists("forward_1.dat") == True
+    assert os.path.exists("backward_1.dat") == True
 
     lqd.run_integration(iteration=2)
-	assert os.path.exists("forward_2.dat") == True
-	assert os.path.exists("backward_2.dat") == True 
+    assert os.path.exists("forward_2.dat") == True
+    assert os.path.exists("backward_2.dat") == True 
 
     lqd.run_integration(iteration=3)
-	assert os.path.exists("forward_3.dat") == True
-	assert os.path.exists("backward_3.dat") == True
+    assert os.path.exists("forward_3.dat") == True
+    assert os.path.exists("backward_3.dat") == True
 
-	lqd.thermodynamic_integration()
-	assert np.abs(lqd.fe - -2.2387655392976304) < 1E-2
+    lqd.thermodynamic_integration()
+    assert np.abs(lqd.fe - -2.2387655392976304) < 1E-2
 
-	lqd.submit_report()
-	assert os.path.exists("report.yaml") == True
+    lqd.submit_report()
+    assert os.path.exists("report.yaml") == True
