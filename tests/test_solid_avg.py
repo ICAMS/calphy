95004261ced4d3c0cb6d969ada4c4ef57566cb0a
srmnitc <sarathmenon@mailbox.org>
2021-01-13 18:38:40 +0100

    update tests

--- tests/test_solid_avg.py
+++ tests/test_solid_avg.py
@@ -7,17 +7,17 @@ import numpy as np
 
 def test_solid_averaging():
     options = read_yamlfile("tests/input.yaml")
-    sol = Solid(t=1000, p=0, l="fcc", apc=4,
-                    alat=4.05, c=0.0, options=options, simfolder=os.getcwd(),
+    sol = Solid(t=1300, p=0, l="fcc", apc=4,
+                    alat=3.704, c=0.0, options=options, simfolder=os.getcwd(),
                     )
     sol.run_averaging()
     assert os.path.exists("msd.dat") == True
 
     sol.gather_average_data()
 
-    assert sol.natoms == 500
-    assert np.abs(sol.alat - 4.144170311277848) < 1E-1
-    assert np.abs(sol.k - 1.6850794001839595) < 1E-1
+    assert sol.natoms == 256
+    assert np.abs(sol.alat - 3.704) < 1E-1
+    assert np.abs(sol.k - 1.31) < 1E-1
 
     sol.run_integration(iteration=1)
     assert os.path.exists("forward_1.dat") == True
@@ -32,7 +32,7 @@ def test_solid_averaging():
     assert os.path.exists("backward_3.dat") == True
 
     sol.thermodynamic_integration()
-    assert np.abs(sol.fe - -3.757705389779369) < 1E-1
+    assert np.abs(sol.fe - -4.00) < 1E-1
 
     sol.submit_report()
     assert os.path.exists("report.yaml") == True
