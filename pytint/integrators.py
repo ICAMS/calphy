135ac2319a43806566577b5e69e6136411acc065
srmnitc <sarathmenon@mailbox.org>
2021-01-20 10:11:26 +0100

    update lqd

--- pytint/integrators.py
+++ pytint/integrators.py
@@ -496,12 +496,12 @@ def integrate_rs(simfolder, f0, t, nsims=5, scale_energy=False,
         cterm = kb*t*np.log(2*np.pi*natoms)/(2*natoms)
     else:
         cterm = 0
-        
+
     wmean = np.mean(ws, axis=0)
     werr = np.std(ws, axis=0)
     temp = t/flambda
 
-    f = f0/flambda - cterm/flambda + 1.5*kb*temp*np.log(flambda) + wmean
+    f = (f0-cterm)/flambda + 1.5*kb*temp*np.log(flambda) + wmean
     
     outfile = os.path.join(simfolder, "reversible_scaling.dat")
     np.savetxt(outfile, np.column_stack((temp, f, werr)))
