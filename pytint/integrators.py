96e06b7ac0e03e379350cf5353bbb01d0a59b226
srmnitc <sarathmenon@mailbox.org>
2021-01-20 11:35:20 +0100

    add option to return values

--- pytint/integrators.py
+++ pytint/integrators.py
@@ -449,7 +449,7 @@ def find_fe(p, x):
 
 
 def integrate_rs(simfolder, f0, t, nsims=5, scale_energy=False, 
-    correction=True, natoms=1):
+    correction=False, natoms=None, return_values=False):
     """
     Carry out the reversible scaling integration
 
@@ -504,5 +504,8 @@ def integrate_rs(simfolder, f0, t, nsims=5, scale_energy=False,
     f = (f0-cterm)/flambda + 1.5*kb*temp*np.log(flambda) + wmean
     warnings.warn("Correcting free energy: org: %f, corr: %f"%(f0, cterm))
 
-    outfile = os.path.join(simfolder, "reversible_scaling.dat")
-    np.savetxt(outfile, np.column_stack((temp, f, werr)))
+    if not return_values:
+        outfile = os.path.join(simfolder, "reversible_scaling.dat")
+        np.savetxt(outfile, np.column_stack((temp, f, werr)))
+    else:
+        return (temp, f, werr)
