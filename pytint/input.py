8e9e1646aaf703005691a8efc3b0502d4a0d78e2
srmnitc <sarathmenon@mailbox.org>
2021-02-01 18:11:22 +0100

    update skeleton of input structure

--- pytint/input.py
+++ pytint/input.py
@@ -4,6 +4,29 @@ Module to handle input files
 import os
 import yaml
 
+def create_calculation_dict():
+    """
+    Create a template for calculation dict
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    dict: dict
+        template dict
+    """
+    cdict = {}
+    cdict["mode"] = "fe"
+    cdict["temperature"] = None
+    cdict["pressure"] = None
+    cdict["lattice"] = None
+    cdict["repeat"] = [7, 7, 7]
+    cdict["state"] = None
+    cdict["nsims"] = 1
+    return cdict
+
 def read_yamlfile(file):
     """
     Read a yaml input file
@@ -16,37 +39,41 @@ def read_yamlfile(file):
     dict: dict
         the read input dict options
     """
+    #there are three blocks - main, md and queue
+    #main block has subblocks of calculations
 
     #we need to set up def options
     options = {}
     
+    #main dictionary
     options["main"] = {
-        "temperature": None,
-        "pressure": 0,
         "element": None,
-        "lattice": None,
-        "concentration": 0,
-        "nsims": 3
+        "mass": 1.00,
     }
 
+    #create a list for calculations
+    options["main"]["calculations"] = []
+
+    #options for md
     options["md"] = {
-        "timestep": 0.001,
+        #pair elements
         "pair_style": None,
         "pair_coeff": None,
+        #time related properties
+        "timestep": 0.001,
         "nsmall": 10000,
         "nevery": 10,
         "nrepeat": 10,
         "ncycles": 100,
-        "mass": None,
+        #ensemble properties
         "tdamp": 0.1,
         "pdamp": 0.1,
-        "nx": 7,
-        "ny": 7,
-        "nz": 7,
+        #eqbr and switching time
         "te": 25000,
         "ts": 50000
     }
 
+    #queue properties
     options["queue"] = {
         "scheduler": "local",
         "cores": 1,
@@ -59,6 +86,7 @@ def read_yamlfile(file):
         "options": None
     }
 
+    #convergence factors that can be set if required
     options["conv"] = {
         "alat_tol": 0.0002,
         "k_tol": 0.01,
