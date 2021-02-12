510c5757acac1ef3b4362d8bbaac8641e9948c46
Sarath Menon <sarath.r.menon@gmail.com>
2021-02-12 19:12:41 +0100

    Revert "update integrators"
    This reverts commit 0d7cba5a8f5755be1662ae0008a3acdd27318fd2.

--- pytint/integrators.py
+++ pytint/integrators.py
@@ -141,10 +141,10 @@ def get_einstein_crystal_fe(temp, natoms, mass, vol, k, concentration, cm_correc
     for count, om in enumerate(omega):
         F_harm += concentration[count]*np.log((hbar*om)/(kb*temp))
         if cm_correction:
-            F_cm += np.log((natoms*concentration[count]/vol)*(2*np.pi*kb*temp/(natoms*concentration[count]*k[count]))**1.5)
-            #F_cm = 0
+            F_cm += np.log((natoms*concentration[count]/vol)*(2*np.pi*kb*temp/(natoms*concentration[count]*mass[count]*om*om))**1.5)
+    
     F_harm = 3*kb*temp*F_harm
-    F_cm = (kb*temp/natoms)*F_cm
+    F_cm = kb*temp*F_cm
 
     F_harm = F_harm + F_cm
 
