cab64f34ed9b87a1d087ca05e7331f65283beea4
srmnitc <sarathmenon@mailbox.org>
2021-02-13 14:45:33 +0100

    add test for integrators

---
+++ tests/test_integrators.py
@@ -0,0 +1,14 @@
+import pytest
+from pytint.integrators import *
+
+def test_ideal_gas():
+	a = get_ideal_gas_fe(1000, 0.07, 1000, [26], [1])
+	assert np.abs(a+0.8900504315410337) < 1E-5
+
+def test_uf():
+	a = get_uhlenbeck_ford_fe(1000, 0.07, 50, 2)
+	assert np.abs(a-5.37158083028874) < 1E-5
+
+def test_solid_ref():
+	a = get_einstein_crystal_fe(1000, 1000, [26], 200, [1.2], [1])
+	assert np.abs(a+0.4727067261423942) < 1E-5
