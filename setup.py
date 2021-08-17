def276b25601bbad64013b9c1b52eae97dff8f1d
srmnitc <sarath.r.menon@gmail.com>
2021-08-17 13:52:18 +0200

    Bump version: 0.5.4 → 0.5.5

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://git.noc.ruhr-uni-bochum.de/atomicclusterexpansion/calphy',
-    version='0.5.4',
+    version='0.5.5',
     zip_safe=False,
     entry_points={
         'console_scripts': [
