3f80278f5836dae98ab45fa166851437dc536b5c
srmnitc <sarath.r.menon@gmail.com>
2021-11-25 09:34:23 +0100

    Bump version: 0.7.2 → 0.7.3

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://git.noc.ruhr-uni-bochum.de/atomicclusterexpansion/calphy',
-    version='0.7.2',
+    version='0.7.3',
     zip_safe=False,
     entry_points={
         'console_scripts': [
