d51f228c2f694e1f0dedb0d1e23d329bfb16a846
srmnitc <sarath.r.menon@gmail.com>
2022-03-11 11:56:20 +0100

    Bump version: 0.9.0 → 0.9.1

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ICAMS/calphy',
-    version='0.9.0',
+    version='0.9.1',
     zip_safe=False,
     entry_points={
         'console_scripts': [
