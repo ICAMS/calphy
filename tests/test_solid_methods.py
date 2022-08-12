0f447593c28bb95a1b5ea4e88aea93806707b8c4
Sarath Menon <sarathmenon@mailbox.org>
2022-08-12 09:26:08 +0200

    update tests

--- tests/test_solid_methods.py
+++ tests/test_solid_methods.py
@@ -1,16 +1,11 @@
 import pytest                                                                                                        
 from calphy.input import read_inputfile                                                                              
 import calphy.lattice as pl                                                                                          
-from calphy.solid import Solid                                                                                       
+from calphy.solid import Solid
+from calphy.liquid import Liquid                                                                                       
 import os                                                                                                            
 import numpy as np                                                                                                   
                                                                                                                      
-def test_solid_averaging_nose_hoover():                                                                                          
-    calculations = read_inputfile(os.path.join(os.getcwd(), "tests/inp1.yaml"))                                     
-    sol = Solid(calculation=calculations[0], simfolder=os.getcwd())                                                  
-    sol.run_averaging()                                                                                              
-    assert os.path.exists("msd.dat") == True
-
 def test_solid_averaging_berendsen_finite_pressure(): 
     calculations = read_inputfile(os.path.join(os.getcwd(), "tests/inp2.yaml"))                                     
     sol = Solid(calculation=calculations[0], simfolder=os.getcwd())
