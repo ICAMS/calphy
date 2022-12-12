bffe0c9e87b195e1f84ffa1e6a92ddcdd66ec2ab
Niklas Leimeroth <leimeroth@mm.tu-darmstadt.de>
2022-12-12 15:30:14 +0100

    Is remapping changes pressure?

--- calphy/helpers.py
+++ calphy/helpers.py
@@ -218,8 +218,8 @@ def set_double_hybrid_potential(lmp, options, ghost_elements=0):
     return lmp
 
 def remap_box(lmp, x, y, z):
-    #lmp.command("run 0")
-    #lmp.command("change_box     all x final 0.0 %f y final 0.0 %f z final 0.0 %f remap units box"%(x, y, z))
+    lmp.command("run 0")
+    lmp.command("change_box     all x final 0.0 %f y final 0.0 %f z final 0.0 %f remap units box"%(x, y, z))
     return lmp
 
 
