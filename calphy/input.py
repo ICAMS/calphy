ea620825d0cde541f671c2565fbb1083913214f6
Sarath Menon <sarathmenon@mailbox.org>
2023-09-26 20:25:11 +0200

    complete update of input class

--- calphy/input.py
+++ calphy/input.py
@@ -22,8 +22,8 @@ sarath.menon@ruhr-uni-bochum.de/yury.lysogorskiy@icams.rub.de
 """
 
 from typing_extensions import Annotated
-from typing import Any, Callable, List
-from pydantic import BaseModel, Field, ValidationError, model_validator, conlist, ClassVar
+from typing import Any, Callable, List, ClassVar, Optional
+from pydantic import BaseModel, Field, ValidationError, model_validator, conlist, PrivateAttr
 from pydantic.functional_validators import AfterValidator, BeforeValidator
 from annotated_types import Len
 
@@ -57,10 +57,10 @@ def to_list(v: Any) -> List[Any]:
     return np.atleast_1d(v)
 
 class CompositionScaling(BaseModel, title='Composition scaling input options'):
-    input_chemical_composition: Annotated[dict, Field(default=None)]
-    output_chemical_composition: Annotated[dict, Field(default=None)]
+    input_chemical_composition: Annotated[dict, Field(default=None, required=False)]
+    output_chemical_composition: Annotated[dict, Field(default=None, required=False)]
     restrictions: Annotated[List[str], BeforeValidator(to_list),
-                                            Field(default=None)]
+                                            Field(default=None, required=False)]
 
 class MD(BaseModel, title='MD specific input options'):
     timestep: Annotated[float, Field(default=0.001, 
@@ -70,8 +70,8 @@ class MD(BaseModel, title='MD specific input options'):
     n_every_steps: Annotated[int, Field(default=10, gt=0)]
     n_repeat_steps: Annotated[int, Field(default=10, gt=0)]
     n_cycles: Annotated[int, Field(default=100, gt=0)]
-    thermostat_damping: Annotated[str, Field(default=0.1, gt=0)]
-    barostat_damping: Annotated[str, Field(default=0.1, gt=0)]
+    thermostat_damping: Annotated[float, Field(default=0.1, gt=0)]
+    barostat_damping: Annotated[float, Field(default=0.1, gt=0)]
     cmdargs: Annotated[str, Field(default=None)]
     init_commands: Annotated[str, Field(default=None)]
 
@@ -107,11 +107,18 @@ class MeltingTemperature(BaseModel, title='Input options for melting temperature
     step: Annotated[int, Field(default=200, ge=20)]
     attempts: Annotated[int, Field(default=5, ge=1)]
 
-class Input(BaseModel, title='Main input class'):
+class Calculation(BaseModel, title='Main input class'):
+    composition_scaling: Optional[CompositionScaling] = CompositionScaling()
+    md: Optional[MD] = MD()
+    nose_hoover: Optional[NoseHoover] = NoseHoover()
+    berendsen: Optional[Berendsen] = Berendsen()
+    queue: Optional[Queue] = Queue()
+    tolerance: Optional[Tolerance] = Tolerance() 
+    melting_temperature: Optional[MeltingTemperature] = MeltingTemperature() 
     element: Annotated[List[str], BeforeValidator(to_list),
                                       Field(default=None)]
     n_elements: Annotated[int, Field(default=None)]
-    mass: Annotated[List[int], BeforeValidator(to_list),
+    mass: Annotated[List[float], BeforeValidator(to_list),
                                       Field(default=None)]
     mode: Annotated[str, Field(default=None)]
     lattice: Annotated[str, Field(default=None)]
@@ -119,26 +126,30 @@ class Input(BaseModel, title='Main input class'):
     #pressure properties
     pressure: Annotated[ None | float | conlist(float, min_length=1, max_length=2) | conlist(conlist(float, min_length=3, max_length=3), min_length=1, max_length=2) , 
                                       Field(default=None)]
-    pressure_stop: ClassVar[float] = None
-    pressure_input: ClassVar[Any] = None
-    iso: ClassVar[bool] = False
-    fix_lattice: ClassVar[bool] = False
+    
+    _pressure_stop: float = PrivateAttr(default=None)
+    _pressure: float = PrivateAttr(default=None) 
+    _pressure_stop: float = PrivateAttr(default=None) 
+    _pressure_input: Any = PrivateAttr(default=None)
+    _iso: bool = PrivateAttr(default=False)
+    _fix_lattice: bool = PrivateAttr(default=False)
 
     temperature: Annotated[ float | conlist(float, min_length=1, max_length=2),
                             Field(default=None)]
-    temperature_high: Annotated[float, Field(default=None)]
-    temperature_stop: ClassVar[float] = None
-    temperature_input: ClassVar[Any] = None
+    _temperature: float = PrivateAttr(default=None)
+    _temperature_high: float = PrivateAttr(default=None)
+    _temperature_stop: float = PrivateAttr(default=None)
+    _temperature_input: float = PrivateAttr(default=None)
     
     melting_cycle: Annotated[ bool, Field(default=True)]
     
-    pair_style: Annotated[ List(str), BeforeValidator(to_list),
+    pair_style: Annotated[ List[str], BeforeValidator(to_list),
                             Field(default=None)]
-    pair_coeff: Annotated[ List(str), BeforeValidator(to_list),
+    pair_coeff: Annotated[ List[str], BeforeValidator(to_list),
                             Field(default=None)]
     potential_file: Annotated[ str, Field(default=None)]
-    pair_style_options: ClassVar[List(str)] = None
-    fix_potential_path: ClassVar[bool] = True
+    _pair_style_options: float = PrivateAttr(default=None)
+    _fix_potential_path: bool = PrivateAttr(default=True)
     
     reference_phase: Annotated[ str, Field(default = None)]
     lattice_constant: Annotated[int, Field(default = 0)]
@@ -153,15 +164,16 @@ class Input(BaseModel, title='Main input class'):
     n_equilibration_steps: Annotated[ int, Field(default = 25000)]
     n_switching_steps: Annotated[ int | conlist(int, min_length=2,
                 max_length=2), Field(default = [50000, 50000])]
-    n_sweep_steps: ClassVar[int] = None
+    _n_switching_steps: int = PrivateAttr(default=50000)
+    _n_sweep_steps: int = PrivateAttr(default=50000)
     n_print_steps: Annotated[int, Field(default = 0)]
     n_iterations: Annotated[int, Field(default = 1)]
     equilibration_control: Annotated[str, Field(default = None)]
     folder_prefix: Annotated[str, Field(default = None)]
 
     #add second level options; for example spring constants
-    spring_constants: Annotated[List(float), Field(default = None)]
-    ghost_element_count: ClassVar[int] = 0
+    spring_constants: Annotated[List[float], Field(default = None)]
+    _ghost_element_count: int = PrivateAttr(default=0)
 
     @model_validator(mode='after')
     def _validate_lengths(self) -> 'Input':
@@ -176,40 +188,41 @@ class Input(BaseModel, title='Main input class'):
 
     @model_validator(mode='after')
     def _validate_pressure(self) -> 'Input':
-        self.pressure_input = copy.copy(self.pressure)
+        self._pressure_input = copy.copy(self.pressure)
         if self.pressure is None:
-            self.iso = True
-            self.fix_lattice = True
-            self.pressure = None
-            self.pressure_stop = None
+            self._iso = True
+            self._fix_lattice = True
+            self._pressure = None
+            self._pressure_stop = None
         elif np.isscalar(self.pressure):
-            self.pressure_stop = self.pressure
-            self.iso = True
-            self.fix_lattice = False
+            self._pressure = self.pressure
+            self._pressure_stop = self.pressure
+            self._iso = True
+            self._fix_lattice = False
         elif np.shape(self.pressure) == (1,):
-            self.iso = True
-            self.fix_lattice = False
-            self.pressure = self.pressure_input[0]
-            self.pressure_stop = self.pressure_input[0]
+            self._iso = True
+            self._fix_lattice = False
+            self._pressure = self.pressure_input[0]
+            self._pressure_stop = self.pressure_input[0]
         elif np.shape(self.pressure) == (2,):
-            self.iso = True
-            self.fix_lattice = False
-            self.pressure = self.pressure_input[0]
-            self.pressure_stop = self.pressure_input[1]
+            self._iso = True
+            self._fix_lattice = False
+            self._pressure = self.pressure_input[0]
+            self._pressure_stop = self.pressure_input[1]
         elif np.shape(self.pressure) == (1, 3):
             if not _check_equal(self.pressure[0]):
                 raise ValueError('All pressure terms must be equal')
-            self.iso = False
-            self.fix_lattice = False
-            self.pressure = self.pressure_input[0][0]
-            self.pressure_stop = self.pressure_input[0][0]                
+            self._iso = False
+            self._fix_lattice = False
+            self._pressure = self.pressure_input[0][0]
+            self._pressure_stop = self.pressure_input[0][0]                
         elif np.shape(self.pressure) == (2, 3):
             if not (_check_equal(self.pressure[0]) and _check_equal(self.pressure[1])):
                 raise ValueError('All pressure terms must be equal')
-            self.iso = False
-            self.fix_lattice = False
-            self.pressure = self.pressure_input[0][0]
-            self.pressure_stop = self.pressure_input[1][0]                                
+            self._iso = False
+            self._fix_lattice = False
+            self._pressure = self.pressure_input[0][0]
+            self._pressure_stop = self.pressure_input[1][0]                                
         else:
             raise ValueError('Unknown format for pressure')
         return self
@@ -217,21 +230,21 @@ class Input(BaseModel, title='Main input class'):
 
     @model_validator(mode='after')
     def _validate_temperature(self) -> 'Input':
-        self.temperature_input = copy.copy(self.temperature)
+        self._temperature_input = copy.copy(self.temperature)
         if self.temperature is None:
-            pass
-        elif shape(np.atleast_1d(self.temperature)) == (1,):
-            temp = self.atleast_1d(self.temperature_input)
-            self.temperature = temp[0]
-            self.temperature_stop = temp[0]
-            self.temperature_high is None:
-                self.temperature_high = 2*temp[0]
-        elif shape(self.temperature) == (2,):
+            self._temperature = None
+        elif np.shape(np.atleast_1d(self.temperature)) == (1,):
+            temp = np.atleast_1d(self.temperature)
+            self._temperature = temp[0]
+            self._temperature_stop = temp[0]
+            if self._temperature_high is None:
+                self._temperature_high = 2*temp[0]
+        elif np.shape(self.temperature) == (2,):
             temp = self.temperature_input
-            self.temperature = temp[0]
-            self.temperature_stop = temp[1]
-            self.temperature_high is None:
-                self.temperature_high = 2*temp[1]
+            self._temperature = temp[0]
+            self._temperature_stop = temp[1]
+            if self._temperature_high is None:
+                self._temperature_high = 2*temp[1]
         return self
 
     @model_validator(mode='after')
@@ -250,17 +263,17 @@ class Input(BaseModel, title='Main input class'):
         self.pair_style = ps_lst
 
         #only set if its None
-        if self.pair_style_options is None:
-            self.pair_style_options = ps_options_lst
+        if self._pair_style_options is None:
+            self._pair_style_options = ps_options_lst
         return self
 
     @model_validator(mode='after')
     def _validate_time(self) -> 'Input':
         if np.isscalar(self.n_switching_steps):
-            self.n_sweep_steps = self.n_switching_steps
+            self._n_sweep_steps = self.n_switching_steps
         else:
-            self.n_sweep_steps = self.n_switching_steps[1]
-            self.n_switching_steps = self.n_switching_steps[0]
+            self._n_sweep_steps = self.n_switching_steps[1]
+            self._n_switching_steps = self.n_switching_steps[0]
         return self
 
 
@@ -367,3 +380,6 @@ def check_dict(indict, key, retval=None):
         return indict[key]
     else:
         return retval
+
+def read_inputfile(file):
+    pass
