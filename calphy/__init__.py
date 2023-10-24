53ceaccb9dd74ab7a9485c57f3226abb4d3fe7c9
Sarath Menon <sarathmenon@mailbox.org>
2023-10-24 13:27:03 +0200

    Bump version: 1.2.13 → 1.2.14

--- calphy/__init__.py
+++ calphy/__init__.py
@@ -4,7 +4,7 @@ from calphy.solid import Solid
 from calphy.alchemy import Alchemy
 from calphy.routines import MeltingTemp
 
-__version__ = "1.2.13"
+__version__ = "1.2.14"
 
 def addtest(a,b):
     return a+b
