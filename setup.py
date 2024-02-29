0686cd0f7c717528adf864c3601a114239c9ac2f
Sarath Menon <sarathmenon@mailbox.org>
2024-02-29 19:19:21 +0100

    Bump version: 1.3.2 → 1.3.3

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     packages=find_packages(include=['calphy', 'calphy.*']),
     test_suite='tests',
     url='https://github.com/ICAMS/calphy',
-    version='1.3.2',
+    version='1.3.3',
     zip_safe=False,
     entry_points={
         'console_scripts': [
