7330c37c3dd919c88f11ed5aa9ac0d9db43f2fa0
Sarath Menon <sarathmenon@mailbox.org>
2024-02-29 23:45:21 +0100

    Bump version: 1.3.3 → 1.3.4

--- calphy/input.py
+++ calphy/input.py
@@ -40,7 +40,7 @@ from pyscal3.core import structure_dict, element_dict, _make_crystal
 from ase.io import read, write
 import shutil
 
-__version__ = "1.3.3"
+__version__ = "1.3.4"
 
 def read_report(folder):
     """
