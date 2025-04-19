cab4b1f457927abe4708e26b49cb5c4afb1ec3c5
Sarath <sarathmenon@mailbox.org>
2025-04-19 23:01:57 +0200

    Bump version: 1.3.13 → 1.4.0

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     packages=find_packages(include=['calphy', 'calphy.*']),
     test_suite='tests',
     url='https://github.com/ICAMS/calphy',
-    version='1.3.13',
+    version='1.4.0',
     zip_safe=False,
     entry_points={
         'console_scripts': [
