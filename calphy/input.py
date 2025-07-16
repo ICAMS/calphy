928582b5b4bcee18bb7e98eb9cf24fa83191fa5b
Sarath Menon <sarathmenon@mailbox.org>
2025-07-16 06:55:05 +0000

    Bump version: 1.4.2 → 1.4.3

--- calphy/input.py
+++ calphy/input.py
@@ -48,7 +48,7 @@ from pyscal3.core import structure_dict, element_dict, _make_crystal
 from ase.io import read, write
 import shutil
 
-__version__ = "1.4.2"
+__version__ = "1.4.3"
 
 
 def _check_equal(val):
