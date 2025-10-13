e806d27a8ea786f8461dcf48de76c7ecb0af7f98
Sarath Menon <sarathmenon@mailbox.org>
2025-10-13 14:36:50 +0200

    add function to parse mp files

--- calphy/input.py
+++ calphy/input.py
@@ -181,11 +181,14 @@ class MeltingTemperature(BaseModel, title="Input options for melting temperature
     attempts: Annotated[int, Field(default=5, ge=1)]
 
 class MaterialsProject(BaseModel, title='Input options for materials project'):
-    api_key: Annotated[str, Field(default="MP_API_KEY")]
+    api_key: Annotated[str, Field(default="")]
     conventional: Annotated[bool, Field(default=True)]
+    target_natoms: Annotated[int, Field(default=1500, description='The structure parsed from materials project would be repeated to approximately this value')]
 
     @field_validator("api_key", mode="after")
     def resolve_api_key(cls, v: str) -> str:
+        if not v:
+            return v
         value = os.getenv(v)
         if not value:
             raise ValueError(
@@ -507,6 +510,37 @@ class Calculation(BaseModel, title="Main input class"):
             # concdict_frac = {str(t): typecounts[c]/np.sum(typecounts) for c, t in enumerate(types)}
             # self._composition = concdict_frac
             # self._composition_counts = concdict_counts
+            self._natoms = len(structure)
+            self._original_lattice = self.lattice.lower()
+            write_structure_file = True
+
+        elif self.lattice.split('-')[0] == 'mp':
+            #confirm here that API key exists
+            if not self.materials_project.api_key:
+                raise ValueError('could not find API KEY, pls set it.')
+            #now we need to fetch the structure
+            try:
+                from mp_api.client import MPRester
+            except ImportError:
+                raise ImportError('Could not import mp_api, make sure you install mp_api package!')
+            #now all good
+            rest = {
+                    "use_document_model": False,
+                    "include_user_agent": True,
+                    "api_key": self.materials_project.api_key, 
+                }
+            with MPRester(**rest) as mpr:
+                docs = mpr.materials.summary.search(material_ids=[self.lattice])
+
+            structures = []
+            for doc in docs:
+                struct = doc['structure']
+                if self.materials_project.conventional:
+                    aseatoms = struct.to_conventional().to_ase_atoms()
+                else:
+                    aseatoms = struct.to_primitive().to_ase_atoms()
+                structures.append(aseatoms)
+            structure = structures[0]          
             self._natoms = len(structure)
             self._original_lattice = self.lattice.lower()
             write_structure_file = True
