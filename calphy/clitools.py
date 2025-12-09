695f903f1f7b38f22494b7e0c777580d54cb44d4
Sarath Menon <sarathmenon@mailbox.org>
2025-12-09 14:18:17 +0100

    minor fix in pd mode

--- calphy/clitools.py
+++ calphy/clitools.py
@@ -125,4 +125,5 @@ def phase_diagram():
     arg = ap.ArgumentParser()
     arg.add_argument("-i", "--input", required=True, type=str,
     help="name of the input file")
+    args = vars(arg.parse_args())
     prepare_inputs_for_phase_diagram(args['input'])
