589647e7c8082bdf81bed8986d97e5630909f589
srmnitc <sarathmenon@mailbox.org>
2025-12-15 15:14:50 +0100

    Bump version: 1.4.6 → 1.4.7

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     packages=find_packages(include=['calphy', 'calphy.*']),
     test_suite='tests',
     url='https://github.com/ICAMS/calphy',
-    version='1.4.6',
+    version='1.4.7',
     zip_safe=False,
     entry_points={
         'console_scripts': [
