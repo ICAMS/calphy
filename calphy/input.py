f614670892070cccc27d612d86e5059b636eaec9
srmnitc <sarathmenon@mailbox.org>
2026-02-04 17:36:40 +0100

    Bump version: 1.5.1 → 1.5.2

--- calphy/input.py
+++ calphy/input.py
@@ -49,7 +49,7 @@ from pyscal3.core import structure_dict, element_dict, _make_crystal
 from ase.io import read, write
 import shutil
 
-__version__ = "1.5.1"
+__version__ = "1.5.2"
 
 
 def _check_equal(val):
